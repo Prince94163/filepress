4890b98f4c36cca4e11863f180a619a3c7033f43
MR-JAGANMOHAN <107793234+MR-JAGANMOHAN@users.noreply.github.com>
2023-06-29 19:11:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,57 +1,13 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from plugins.helper import START_TXT
 import aiohttp
-import requests
-import json
-
-START_TXT = "Hey {mention}, I will provide you the Filepress Links and Shortened GyaniLinks :)\n\n--> Set API using  /setapi <your gyanilinks api>\n-->send the link to bot and see the magic"
+from plugins.filepress import get_filepress
 
 api_id = 13115322
 api_hash = "f28fbd1367ddda2e6f863c3129323743"
 bot_token = "5921362645:AAEOVznXBTwqx6XaASwN855j1rKmlN19Ef8"
 bot = Client("bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token,workers=50,sleep_threshold=10)
-url = "https://filepress.cfd/api/v1/file/add"
-api = "OWn5m6d5yvZGOuUgkYMTeqdVnmNPuCVJ42LSimkb1uw="
-
-async def get_filepress(link):
-        async def extract_file_id(url):
-            file_id = None
-
-            try:
-                parts = url.split('/')
-                file_id = parts[-2]  # Get the second-to-last part
-                
-                # If the file ID contains a query parameter, remove it
-                if '?' in file_id:
-                    file_id = file_id.split('?')[0]
-                
-                # If the file ID contains an 'open?id=' keyword, remove it
-                if 'open?id=' in file_id:
-                    file_id = file_id.replace('open?id=', '')
-
-            except Exception as e:
-                print("Error extracting file ID:", e)
-
-            return file_id
-
-        file_id = await extract_file_id(link)
-
-        payload = {
-            "key": api,
-            "id": file_id
-        }
-        headers = {}
-        response = requests.post(url, headers=headers, json=payload)
-        response_text = response.text
-        data = json.loads(response_text)
-
-        fp_id = data["data"]["_id"]
-        fp_url = "https://filepress.cfd/file/" + fp_id
-
-        file_name = data["data"]["name"]
-        file_size = data["data"]["size"]
-        
-        return fp_url, file_name, file_size
 
 @bot.on_message(filters.command(["start", "help"]) & filters.private)
 async def welcome(client, message):
