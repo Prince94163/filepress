d4f4fd54250321783c39e13d7f654eae7953b964
Prince94163 <128693609+Prince94163@users.noreply.github.com>
2024-07-04 15:44:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,10 @@ api_id = 24736263
 api_hash = "4d53732917b73a6bb89c3b2f2f7b0902"
 bot_token = "6683767504:AAFSYlmoMbfCXGAq3qHMDQwdIUUHzy7-TrM"
 bot = Client("bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token,workers=50,sleep_threshold=10)
+
 api = str(None)
+gdtot_api = str(None)
+filepress_api = str(None)
 
 
 ########################################################################
@@ -27,20 +30,42 @@ async def welcome(client, message):
 
 
 ########################################################################
-@bot.on_message(filters.command(["setapi"]) & filters.private)
+@bot.on_message(filters.command(["setapi_filepress"]) & filters.private)
 async def set_api(client, message):
-    global api
+    global filepress_api
     try:
-        api = message.command[1]
-        await message.reply_text(f"You have set your api successfully as\n\n<code>{api}</code>")
+        filepress_api = message.command[1]
+        await message.reply_text(f"You have set your api successfully as\n\n<code>{filepress_api}</code>")
     except IndexError:
-        await message.reply_text(f"PUT API KEY WITH COMMAND\n\nExample:- /setapi cuigwudfgywg156235vbchbgujhfgu")
+        await message.reply_text(f"PUT API KEY WITH COMMAND\n\nExample:- /setapi_filepress cuigwudfgywg156235vbchbgujhfgu")
+
+
+
+@bot.on_message(filters.command(["setapi_gdtot"]) & filters.private)
+async def set_api(client, message):
+    global gdtot_api
+    try:
+        gdtot_api = message.command[1]
+        await message.reply_text(f"You have set your api successfully as\n\n<code>{gdtot_api}</code>\n\nDon't forget to add GDTOT Email by use /setgdtot_mail command")
+    except IndexError:
+        await message.reply_text(f"PUT API KEY WITH COMMAND\n\nExample:- /setapi_gdtot cuigwudfgywg156235vbchbgujhfgu")
+
+@bot.on_message(filters.command(["setgdtot_mail"]) & filters.private)
+async def set_mail(client, message):
+    global GDTOT_EMAIL
+    try:
+        GDTOT_EMAIL = message.command[1]
+        await message.reply_text(f"You have set your Mail successfully as\n\n<code>{GDTOT_EMAIL}</code>\n\nDon't forget to add GDTOT api by use /setapi_gdtot command")
+    except IndexError:
+        await message.reply_text(f"PUT API KEY WITH COMMAND\n\nExample:- /setapi_gdtot cuigwudfgywg156235vbchbgujhfgu")
+
 ########################################################################
 
 
 
 ###################### FILEPRESS ######################
-FILEPRESS_URL = environ.get("FILEPRESS_URL", "https://filebee.xyz/api/v1/file/add")
+FILEPRESS_DOMAIN = environ.get("FILEPRESS_DOMAIN", "filebee.xyz")
+FILEPRESS_URL = f"https://{FILEPRESS_DOMAIN}/api/v1/file/add"
 
 async def get_filepress(link):
         async def extract_file_id(url):
@@ -66,22 +91,65 @@ async def get_filepress(link):
         file_id = await extract_file_id(link)
 
         payload = {
-            "key": api,
+            "key": filepress_api,
             "id": file_id
         }
         headers = {}
-        response = requests.post(url, headers=headers, json=payload)
+        response = requests.post(FILEPRESS_URL, headers=headers, json=payload)
         response_text = response.text
         data = json.loads(response_text)
 
         fp_id = data["data"]["_id"]
-        fp_url = "https://filebee.xyz/file/" + fp_id
+        fp_url = f"https://{FILEPRESS_DOMAIN}/file/" + fp_id
 
         file_name = data["data"]["name"]
         file_size = data["data"]["size"]
         
         return fp_url, file_name, file_size
 ###################### FILEPRESS ######################
+
+###################### GDTOT ######################
+GDTOT_DOMAIN = environ.get("GDTOT_DOMAIN", "new4.gdtot.dad")
+GDTOT_URL = f"https://{GDTOT_DOMAIN}/api/upload/link"
+
+async def get_gdtot(link):
+        async def extract_file_id(url):
+            file_id = None
+
+            try:
+                parts = url.split('/')
+                file_id = parts[-2]  # Get the second-to-last part
+                
+                # If the file ID contains a query parameter, remove it
+                if '?' in file_id:
+                    file_id = file_id.split('?')[0]
+                
+                # If the file ID contains an 'open?id=' keyword, remove it
+                if 'open?id=' in file_id:
+                    file_id = file_id.replace('open?id=', '')
+
+            except Exception as e:
+                print("Error extracting file ID:", e)
+
+            return file_id
+
+        file_id = await extract_file_id(link)
+
+        payload = {
+            "api_token": gdtot_api,
+            "email": GDTOT_EMAIL,
+            "url": file_id
+        }
+        headers = {}
+        response = requests.post(GDTOT_UR, headers=headers, json=payload)
+        response_text = response.text
+        data = json.loads(response_text)
+
+        gd_id = data["data"]["id"]
+        gd_url = f"https://{GDTOT_DOMAIN}/file/" + gd_id
+        
+        return gd_url
+###################### GDTOT ######################
 
 
 
@@ -92,9 +160,9 @@ async def link_handler(bot, message):
     try:
         if link.startswith("https://drive.google.com") or link.startswith("http://drive.google.com") or link.startswith("drive.google.com"):
             fp = await get_filepress(link)
-            if fp[0] != "":
-                short_link = await get_shortlink(fp[0])
-                await message.reply(f"<b>FILE NAME:-</b> <code>{fp[1]}</code>\n\n<b>FILE SIZE:-</b> <code>{fp[2]}</code>\n\n<b>FilePress: </b><code>{fp[0]}</code>\n\n<b>GyaniLinks: </b><code>{short_link}</code>")
+            gd = await get_gdtot(link)
+            if fp[0] and gd[0] != "":
+                await message.reply(f"<b>FILE NAME:-</b> <code>{fp[1]}</code>\n\n<b>FILE SIZE:-</b> <code>{fp[2]}</code>\n\n<b>FilePress: </b><code>{fp[0]}</code>\n\n<b>GDTOT: </b><code>{gd[0]}</code>")
     except Exception as e:
         await message.reply(f'Error: {e}', quote=True)
 ########################################################################
